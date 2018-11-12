e2b0cd9544827233d1f69971f7a2f1192f159591
gyozke <gyozke@pc9w10.inf.elte.hu>
2018-11-12 11:35:03 +0100

    dynamic issue list, status-filter component, issue-form prepared

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Issue } from '../issue';
 
 @Component({
   selector: 'issue-list',
@@ -7,9 +8,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IssueListComponent implements OnInit {
 
+  issues: Issue[] = [
+    {
+      id: 1,
+      title: 'Rossz a gép',
+      place: 'PC6',
+      description: 'Valami leírás',
+      status: 'NEW',
+      updated_at: '2018-11-11',
+    },
+    {
+      id: 2,
+      title: 'Rossz a gép',
+      place: 'PC6',
+      description: 'Valami leírás',
+      status: 'DOING',
+      updated_at: '2018-11-11'
+    },{
+      id: 3,
+      title: 'Rossz a gép',
+      place: 'PC6',
+      description: 'Valami leírás',
+      status: 'DOING',
+      updated_at: '2018-11-11'
+    },{
+      id: 4,
+      title: 'Rossz a gép',
+      place: 'PC6',
+      description: 'Valami leírás',
+      status: 'DONE',
+      updated_at: '2018-11-11'
+    },
+  ]
+  filteredIssues = [];
+  selectedStatus = 'DONE';
+  selectedIssue = null;
+
   constructor() { }
 
   ngOnInit() {
+    this.filterIssues();
+  }
+
+  filterIssues() {
+    this.filteredIssues = this.selectedStatus === ''
+      ? this.issues
+      : this.issues.filter(issue => issue.status === this.selectedStatus);
+  }
+
+  onFilterChange(data) {
+    this.selectedStatus = data;
+    this.filterIssues();
   }
 
 }
